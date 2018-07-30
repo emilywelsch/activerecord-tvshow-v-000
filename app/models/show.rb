063807432a1c40ab5aaf5015abcb870ed3f0e515
Emily Welsch <emwelsch@gmail.com>
2018-07-30 02:16:07 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,9 @@
 class Show < ActiveRecord::Base
 
-  # @rick_and_morty = Show.create(:name => "Rick and Morty", :day => "Thursday", :network => "Adult Swim", :rating => 10)
-  # @law_and_order = Show.create(:name => "Law & Order", :day => "Monday", :network => "NBC", :rating => 7)
-  # @the_cleveland_show = Show.create(:name => "The Cleveland Show", :day => "Monday", :network => "Fox", :rating => 2)
-  # @fear_the_walking_dead = Show.create(:name => "Fear the Walking Dead", :day => "Sunday", :network => "AMC", :rating => 3)
+  @rick_and_morty = Show.create(:name => "Rick and Morty", :day => "Thursday", :network => "Adult Swim", :rating => 10)
+  @law_and_order = Show.create(:name => "Law & Order", :day => "Monday", :network => "NBC", :rating => 7)
+  @the_cleveland_show = Show.create(:name => "The Cleveland Show", :day => "Monday", :network => "Fox", :rating => 2)
+  @fear_the_walking_dead = Show.create(:name => "Fear the Walking Dead", :day => "Sunday", :network => "AMC", :rating => 3)
 
   def highest_rating
     Show.maximum(:rating)
@@ -14,13 +14,14 @@ class Show < ActiveRecord::Base
   end
 
   def lowest_rating #returns the lowest value in the ratings column.
+    Show.minimum(:rating)
   end
 
   def least_popular_show #returns the show with the lowest rating.
   end
 
   def ratings_sum #returns the sum of all of the ratings.
-    Show.sum(:ratings)
+    Show.sum(:rating)
   end
 
   def popular_shows #returns an array of all of the shows that have a rating greater than 5. hint: use the where Active Record method.
