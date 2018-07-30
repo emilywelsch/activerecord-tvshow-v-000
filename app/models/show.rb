da7638bbd17128386cacb6a188252e421418093f
Emily Welsch <emwelsch@gmail.com>
2018-07-30 02:31:03 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show #this method should return the show with the highest rating.
-    Show.find_by(rating:highest_rating).name
+    Show.find_by(rating:highest_rating)#.name
   end
 
   def self.lowest_rating
@@ -13,7 +13,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    Show.find_by(rating:lowest_rating).name
+    Show.find_by(rating:lowest_rating)#.name
   end
 
   def self.ratings_sum
