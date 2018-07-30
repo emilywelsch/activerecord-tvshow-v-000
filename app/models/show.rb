c11c27bd33bca837384591821d143add3d9305e1
Emily Welsch <emwelsch@gmail.com>
2018-07-30 02:05:00 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
 
   def most_popular_show #this method should return the show with the highest rating. hint: use the highest_rating method as a helper method.
     Show.find_by(rating:10).name
-    Show.find_by(rating: highest_rating).name
+    Show.find_by(rating: Show.highest_rating).name
 
   end
 
